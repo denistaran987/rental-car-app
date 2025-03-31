6db4361f5d452ca6507a8fa05d9745f851ca9083
Denis <denistaran482@gmail.com>
2025-04-01 01:35:54 +0300

    Add library and set up Redux store

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster position="top-right" reverseOrder={false} />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
